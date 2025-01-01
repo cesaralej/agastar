0c5ecfe1f233ac993ec10813f8802266aea6e11b
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2025-01-01 15:25:51 +0100

    Form updates

--- src/components/NewTransaction.tsx
+++ src/components/NewTransaction.tsx
@@ -23,6 +23,14 @@ import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
 
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+
 const schema = z.object({
   amount: z.number().positive("Amount must be a positive number"),
   description: z.string(),
@@ -92,6 +100,35 @@ const NewTransaction = () => {
                   <FormControl>
                     <Input placeholder="e.g., Coffee at Starbucks" {...field} />
                   </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="comment"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email</FormLabel>
+                  <Select
+                    onValueChange={field.onChange}
+                    defaultValue={field.value}
+                  >
+                    <FormControl>
+                      <SelectTrigger>
+                        <SelectValue placeholder="Select a verified email to display" />
+                      </SelectTrigger>
+                    </FormControl>
+                    <SelectContent>
+                      <SelectItem value="m@example.com">
+                        m@example.com
+                      </SelectItem>
+                      <SelectItem value="m@google.com">m@google.com</SelectItem>
+                      <SelectItem value="m@support.com">
+                        m@support.com
+                      </SelectItem>
+                    </SelectContent>
+                  </Select>
                   <FormMessage />
                 </FormItem>
               )}
