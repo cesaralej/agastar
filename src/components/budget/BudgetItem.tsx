571ac0ff82369119e3f93d147e9820854b9986f5
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2025-04-12 20:00:19 +0200

    [UI] Number commas

--- src/components/budget/BudgetItem.tsx
+++ src/components/budget/BudgetItem.tsx
@@ -11,6 +11,11 @@ interface BudgetItemProps {
   noEdit?: boolean;
 }
 
+const formatNumberWithCommas = (number: number | undefined): string => {
+  if (number === undefined) return "0";
+  return number.toFixed(0).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+};
+
 const BudgetItem = ({
   budget,
   spent,
@@ -106,11 +111,13 @@ const BudgetItem = ({
           />
         ) : (
           <>
-            <p className="text-sm text-gray-500">Budget: {budget.amount}€</p>
+            <p className="text-sm text-gray-500">
+              Budget: {formatNumberWithCommas(budget.amount)}€
+            </p>
             <div className="flex justify-between items-center">
               <div>
                 <p className={`text-sm ${getRemainingColor(remaining)}`}>
-                  Remaining: {remaining}€
+                  Remaining: {formatNumberWithCommas(remaining)}€
                 </p>
               </div>
               <div className="w-2/5">
