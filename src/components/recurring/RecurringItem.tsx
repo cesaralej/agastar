eb5439bf8c64efaa5d30ed4df38e94424762df39
Cesar Garcia <43119240+cesaralej@users.noreply.github.com>
2025-04-17 11:27:40 +0200

    [UI] Recurring card update

--- src/components/recurring/RecurringItem.tsx
+++ src/components/recurring/RecurringItem.tsx
@@ -1,12 +1,29 @@
 import React, { useEffect, useState } from "react";
 import { Recurring } from "@/types";
 import { FaCreditCard, FaWallet } from "react-icons/fa";
+import { FaExclamationTriangle, FaCheckCircle, FaClock } from "react-icons/fa";
 import { Timestamp } from "firebase/firestore";
 import { formatCurrency } from "@/lib/utils";
 
 interface RecurringItemProps {
   recurring: Recurring;
   onEdit: (recurring: Recurring) => void;
+}
+
+function formatDateWithSuffix(day: number): string {
+  if (day >= 11 && day <= 13) {
+    return day + "th";
+  }
+  switch (day % 10) {
+    case 1:
+      return day + "st";
+    case 2:
+      return day + "nd";
+    case 3:
+      return day + "rd";
+    default:
+      return day + "th";
+  }
 }
 
 const RecurringItem = ({ recurring, onEdit }: RecurringItemProps) => {
@@ -58,6 +75,20 @@ const RecurringItem = ({ recurring, onEdit }: RecurringItemProps) => {
   const accountIcon =
     recurring.account === "savings" ? <FaWallet /> : <FaCreditCard />;
 
+  const statusIcon = dueStatus.includes("Overdue") ? (
+    <FaExclamationTriangle className="text-red-500" />
+  ) : dueStatus.includes("Paid") ? (
+    <FaCheckCircle className="text-green-500" />
+  ) : (
+    <FaClock className="text-yellow-500" />
+  );
+
+  const bgColor = dueStatus.includes("Overdue")
+    ? "bg-red-100"
+    : dueStatus.includes("Paid")
+    ? "bg-green-100"
+    : "bg-gray-100";
+
   return (
     <div
       className={`rounded-lg shadow-md p-4 bg-white hover:shadow-lg hover:scale-101 transition-all duration-200`}
@@ -67,28 +98,31 @@ const RecurringItem = ({ recurring, onEdit }: RecurringItemProps) => {
         aria-label={`Recurring`}
         className="border-none bg-transparent text-left w-full h-full p-0 block"
       >
-        <div className="flex items-center justify-between">
-          <span className="text-lg font-semibold">
-            {truncateText({ text: recurring.description, limit: 20 })}
-          </span>{" "}
-          <span className={`text-lg font-semibold text-right`}>
-            {formatCurrency(Number(recurring.amount))}
-          </span>
-        </div>
-
-        <div
-          className={`flex items-center gap-2 text-sm text-gray-600 ${
-            dueStatus.includes("Paid") ? "text-green-500" : ""
-          } ${dueStatus.includes("Overdue") ? "text-red-500" : ""}`}
-        >
-          {accountIcon}
-          <p
-            className={`text-sm ${
-              dueStatus.includes("Overdue") ? "text-red-500" : "text-gray-500"
-            } ${dueStatus.includes("Paid") ? "text-green-500" : ""}`}
+        <div className="flex  gap-4">
+          <div
+            className={`w-12 h-12 rounded-full flex items-center ${bgColor} justify-center shrink-0`}
           >
-            {dueStatus}
-          </p>
+            {statusIcon}
+          </div>
+          <div className="flex-grow">
+            <div className="flex items-center justify-between">
+              <span className="text-lg font-semibold">
+                {truncateText({ text: recurring.description, limit: 20 })}
+              </span>{" "}
+              <span className={`text-lg font-semibold text-right`}>
+                {formatCurrency(Number(recurring.amount))}
+              </span>
+            </div>
+            <div className={`flex items-center justify-between`}>
+              <div className={`flex items-center gap-2 text-sm text-gray-600 `}>
+                {accountIcon}
+                <p>{dueStatus}</p>
+              </div>
+              <span className={`text-sm text-gray-600 `}>
+                {formatDateWithSuffix(recurring.dueDate)}
+              </span>
+            </div>
+          </div>
         </div>
       </button>
     </div>
